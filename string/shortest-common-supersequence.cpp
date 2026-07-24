9e89912c236e524a8ca67fd0dddbcb5a948dc976
Shraddha Bhatt <techsince2025@gmail.com>
2026-07-24 18:54:15 +0530

    LC-shortest-common-supersequence [Runtime: N/A; Memory: N/A]

--- string/shortest-common-supersequence.cpp
+++ string/shortest-common-supersequence.cpp
@@ -1,39 +1,62 @@
 class Solution {
 public:
-
-    string func(int i, int j, string& s1, string& s2, vector<vector<string>>& dp){
-
-    if(i == s1.size()){
-        return s2.substr(j);
-    }
-
-    if(j == s2.size()){
-        return s1.substr(i);
-    }
-
-    if(dp[i][j] != "#"){
-        return dp[i][j];
-    }
-
-    if(s1[i] == s2[j]){
-        return dp[i][j] = s1[i]+ func(i+1, j+1, s1, s2, dp);
-    }
-
-    string notmatch1 = string(1, s1[i]) + func(i+1, j, s1, s2, dp);
-    string notmatch2 = string(1, s2[j]) + func(i, j+1, s1, s2, dp);
-
-
-    if(notmatch1.size() >= notmatch2.size()){
-        return dp[i][j] =  notmatch2;
-    }
-    
-    return dp[i][j] = notmatch1;
-
-    }
     string shortestCommonSupersequence(string str1, string str2) {
-        
-        vector<vector<string>> dp(str1.size(), vector<string>(str2.size(), "#"));
+        int n = str1.size();
+        int m = str2.size();
 
-        return func(0,0, str1, str2, dp);
+        //LCS Tabulation
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=1; i <= n; i++){
+            for(int j=1; j <= m; j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        //Backtracking
+        int i=n;
+        int j=m;
+
+        string ans = "";
+
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans = ans+str2[j-1];
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j] > dp[i][j-1]){
+                    ans = ans + str1[i-1];
+                    i--;
+                
+
+            }
+
+            else {
+                ans = ans + str2[j-1];
+                j--;
+            }
+        }
+
+        //Remaining characters of str1
+
+        while(i > 0){
+            ans = ans + str1[i-1];
+            i--;
+        }
+
+        while(j > 0){
+            ans = ans + str2[j-1];
+            j--;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
