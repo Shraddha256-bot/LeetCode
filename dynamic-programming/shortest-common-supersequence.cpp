9916e016a0b967cbfef144f38e5e1d1f2ca6a541
Shraddha Bhatt <techsince2025@gmail.com>
2026-07-24 16:12:12 +0530

    LC-shortest-common-supersequence [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/shortest-common-supersequence.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string func(int i, int j, string& s1, string& s2, vector<vector<string>>& dp){
+
+    if(i == s1.size() || j == s2.size()){
+        return "";
+    }
+
+    if(dp[i][j] != "#"){
+        return dp[i][j];
+    }
+
+    if(s1[i] == s2[j]){
+        string match = s1[i]+ func(i+1, j+1, s1, s2, dp);
+    }
+
+    string notmatch1 = s1[i] + func(i+1, j, s1, s2, dp);
+    string notmatch2 = s2[j] + func(i, j+1, s1, s2, dp);
+
+
+    if(notmatch1.size() >= notmatch2.size()){
+        return notmatch2;
+    }
+    
+    return notmatch1;
+
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+        vector<vector<string>> dp(str1.size(), vector<string>(str2.size(), "#"));
+
+        return func(0,0, str1, str2, dp);
+    }
+};
